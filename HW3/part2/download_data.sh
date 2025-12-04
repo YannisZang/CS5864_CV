a477b7b6f7cbe71f4a1194fba6a6c242b9a2a224
Yannis Zang <xiaoyanzang24@hotmail.com>
2025-12-03 20:18:56 -0500

    Add files via upload

---
+++ HW3/part2/download_data.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+cd data
+
+# download train
+wget https://thor.robots.ox.ac.uk/pascal/VOC/voc2007/VOCtrainval_06-Nov-2007.tar
+tar -xf VOCtrainval_06-Nov-2007.tar
+mv VOCdevkit VOCdevkit_2007
+rm VOCtrainval_06-Nov-2007.tar
+
+# download test and combine into same directory
+wget https://thor.robots.ox.ac.uk/pascal/VOC/voc2007/VOCtest_06-Nov-2007.tar
+tar -xf VOCtest_06-Nov-2007.tar
+mv VOCdevkit/VOC2007 VOCdevkit_2007/VOC2007test
+rmdir VOCdevkit
+rm VOCtest_06-Nov-2007.tar
+
